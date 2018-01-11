836b9eb1a2af6ae96031210a7c9ce74fb8dc25f0
Leslie Merchan <lesliemerchan71@gmail.com>
2018-01-11 23:02:36 +0000

    put in input and button and title and styled a little

--- js/script.js
+++ js/script.js
@@ -6,8 +6,16 @@
 
 // Document Ready Function. All of your jQuery should go in here. 
 $( document ).ready(function() {
-  
+ 
+  $("button").click(function(){
+    var message=$("input").val();
+    var translatedMessage = ay(message);
+    $("#result").text(translatedMessage);
+});
 
+function ay(word){
+ return word + "ay";    
+}
 
 
 });
