33525b406aa9f9dd9b6364ccfa51b5edf54c586b
Leslie Merchan <lesliemerchan71@gmail.com>
2018-02-08 22:56:36 +0000

    was able to use vowels into piglatin

--- js/script.js
+++ js/script.js
@@ -4,53 +4,42 @@
 // igpay, banana becomes ananabay, and aadvark becomes aadvarkay.
 // CREATE THE FUNCTIONS BELOW
 
+/* global $*/
 // Document Ready Function. All of your jQuery should go in here. 
-$( document ).ready(function() {
- 
-  $("button").click(function(){
-    var message=$("input").val();
-    var translatedMessage = sentenceToPigLatin(message);
+$(document).ready(function() {
+  $("button").click(function() {
+    var message = $("input").val();
+    var translatedMessage = wordToPigLatin(message);
     $("#result").text(translatedMessage);
+  });
 });
 
-$("button").click(function(){
-    var message=$("input").val();
-    var translatedMessage = ayToPigLatin(message);
-    $("#result").text(translatedMessage);
-});
+// isVowel("f") -> false
+// isVowel("a") -> true
+// isVowel("e") -> true
+function isVowel(character) {
+  var lower = ["a", "e", "i", "o", "u"];
 
-$("button").click(function(){
-    var message=$("input").val();
-    var translatedMessage = PigLatin(message);
-    $("#result").text(translatedMessage);
-});
-
-function ay(word){
- return word + "ay";    
+  for (var i = 0; i <= lower.length; i++) {
+    var vowel = lower[i];
+    if (character == vowel) {
+      return true;
+    }
+  }
+  return false;
 }
 
-function sentenceToPigLatin(message){
-  return message.split(" ");
-}
+  // Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
+  function wordToPigLatin(word) {
+    if (isVowel(word[0])) {
+      return word + "yay";
+    }
+    else {
+      return word;
+    }
+  }
+  
 
-function ayToPigLatin(message){
-  return message.split(" "); 
-}
-
-function 
-
-});
-
-
-// Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
-
-
-
-
-
-
-// Create the sentenceToPigLatin function that takes a sentence as a parameter
-	//Loops through all the words in the sentence and transforms each word
-	//It should return a transfromed sentance
-
-
+  // Create the sentenceToPigLatin function that takes a sentence as a parameter
+  //Loops through all the words in the sentence and transforms each word
+  //It should return a transfromed sentance
